629f7b3f41de1d75cb9b680826d40c4788dc6f60
Thiguet <thiguet2014@gmail.com>
2020-09-18 09:14:35 -0300

    Fixed snapshots and some package.json scripts.

--- tests/unit/components/Input.spec.ts
+++ tests/unit/components/Input.spec.ts
@@ -3,83 +3,71 @@ import Input from '@/components/Input.vue';
 import faker from 'faker';
 
 interface InputData {
-  id: string;
-  label: string;
-  type: string;
-  value: string;
-  setValue: Function;
-  legend?: string;
+    id: string;
+    label: string;
+    type: string;
+    value: string;
+    setValue: Function;
+    legend?: string;
 }
 
 const props: InputData = {
-  id: faker.random.alphaNumeric(),
-  type: 'text',
-  value: faker.random.word(),
-  setValue: jest.fn(),
-  label: faker.random.word(),
-  legend: faker.random.word(),
+    id: faker.random.alphaNumeric(),
+    type: 'text',
+    value: faker.random.word(),
+    setValue: jest.fn(),
+    label: faker.random.word(),
+    legend: faker.random.word(),
 };
 
 describe('Input.vue', () => {
-  const build = () => {
-    const wrapper = shallowMount(Input, {
-      propsData: { ...props },
+    const build = () => {
+        const wrapper = shallowMount(Input, {
+            propsData: { ...props },
+        });
+
+        return {
+            wrapper,
+            input: () => wrapper.find('.input'),
+            labelEl: () => wrapper.find('.label'),
+        };
+    };
+
+    it('renders component', () => {
+        props.id = 'someFixedValidId';
+        props.legend = 'someFixedLegend';
+        props.value = 'someFixedValidValue';
+        props.label = 'someFixedValidLabel';
+
+        const { wrapper } = build();
+        expect(wrapper).toMatchSnapshot();
     });
 
-    return {
-      wrapper,
-      input: () => wrapper.find('.input'),
-      labelEl: () => wrapper.find('.label'),
-    };
-  };
+    it('renders main child components', () => {
+        const { input, labelEl } = build();
 
-  it('renders component', () => {
-    props.id = 'someFixedValidId';
-    props.value = 'someFixedValidValue';
-    props.label = 'someFixedValidLabel';
+        expect(input().exists()).toBe(true);
+        expect(labelEl().exists()).toBe(true);
+    });
 
-    const { wrapper } = build();
-    expect(wrapper).toMatchSnapshot();
-  });
+    it('passes props to the component', () => {
+        const { wrapper } = build();
 
-  it('renders main child components', () => {
-    const {
-      input,
-      labelEl,
-    } = build();
+        const { type, value, label, id, setValue, legend } = wrapper.props();
 
-    expect(input().exists()).toBe(true);
-    expect(labelEl().exists()).toBe(true);
-  });
+        expect(id).toBe(props.id);
+        expect(type).toBe(props.type);
+        expect(value).toBe(props.value);
+        expect(label).toBe(props.label);
+        expect(setValue).toBe(props.setValue);
+        expect(legend).toBe(props.legend);
+    });
 
-  it('passes props to the component', () => {
-    const { wrapper } = build();
+    it('sets label propety to label element', () => {
+        const { wrapper, labelEl } = build();
 
-    const {
-      type,
-      value,
-      label,
-      id,
-      setValue,
-      legend,
-    } = wrapper.props();
+        const { label } = wrapper.props();
 
-    expect(id).toBe(props.id);
-    expect(type).toBe(props.type);
-    expect(value).toBe(props.value);
-    expect(label).toBe(props.label);
-    expect(setValue).toBe(props.setValue);
-    expect(legend).toBe(props.legend);
-  });
-
-  it('sets label propety to label element', () => {
-    const {
-      wrapper,
-      labelEl,
-    } = build();
-
-    const { label } = wrapper.props();
-
-    expect(labelEl().text()).toBe(label);
-  });
+        expect(labelEl().text()).toBe(label);
+    });
 });
