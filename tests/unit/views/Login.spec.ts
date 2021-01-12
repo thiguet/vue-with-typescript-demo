7f51fc08aa293d22b391d395684852a3e3d6e422
Thiguet <thiguet2014@gmail.com>
2021-01-11 23:20:39 -0300

    Refactored login tests.

--- tests/unit/views/Login.spec.ts
+++ tests/unit/views/Login.spec.ts
@@ -1,195 +1,60 @@
-import { shallowMount, mount, createLocalVue, Wrapper } from '@vue/test-utils';
+import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
 
-import Vuex, { Store, ModuleTree } from 'vuex';
+import Vuex from 'vuex';
 
 import Login from '@/views/Login.vue';
 
-import Button from '@/components/Button.vue';
-import TextInput from '@/components/TextInput.vue';
-
-import faker from 'faker';
-import {
-    ActionTypes,
-    MutationTypes,
-    State as LoginState,
-} from '@/store/modules/login';
-
-import { LoginView } from '@/views/models.d';
-
-import { LoginVuex } from '../store/models.d';
-
-import { VuexAppModules } from '@/store/datatypes/models';
+import { baseURL } from '@/services/config.json';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('Login', () => {
-    let login: LoginVuex;
-    let store: Store<LoginState>;
-
     const build = () => {
         const wrapper: Wrapper<Login> = shallowMount(Login, {
-            localVue,
-            store,
-        });
-
-        const mountedWrapper: Wrapper<Login> = mount(Login, {
-            localVue,
-            store,
+            data: () => ({ baseURL }),
         });
 
         return {
             wrapper,
-            mountedWrapper,
-            wrapperVM: (wrapper.vm as unknown) as LoginView,
-            name: () => mountedWrapper.findAllComponents(TextInput).at(0),
-            pass: () => mountedWrapper.findAllComponents(TextInput).at(1),
-            btn: () => mountedWrapper.findComponent(Button),
-            facebookIcon: () => mountedWrapper.find('#facebook'),
-            githubIcon: () => mountedWrapper.find('#github'),
-            googleIcon: () => mountedWrapper.find('#google'),
+            facebookIcon: () => wrapper.find('#facebook'),
+            githubIcon: () => wrapper.find('#github'),
+            googleIcon: () => wrapper.find('#google'),
         };
     };
 
-    beforeEach(() => {
-        const name = faker.internet.userName();
-
-        login = {
-            namespaced: true,
-            state: {
-                currentUser: {
-                    id: faker.random.uuid(),
-                    name,
-                    email: name,
-                },
-                username: name,
-                password: faker.internet.password(),
-            },
-            mutations: {
-                [MutationTypes.setUsername]: jest.fn(),
-                [MutationTypes.setPassword]: jest.fn(),
-            },
-            actions: {
-                [ActionTypes.loginAction]: jest.fn(),
-                [ActionTypes.facebookLogin]: jest.fn(),
-                [ActionTypes.githubLogin]: jest.fn(),
-                [ActionTypes.googleLogin]: jest.fn(),
-            },
-        };
-
-        const modules: ModuleTree<LoginState> = {
-            [VuexAppModules.login]: {
-                ...login,
-            },
-        };
-
-        store = new Vuex.Store({
-            modules,
-        });
-    });
-
     it('renders component', () => {
-        login.state.currentUser = {
-            id: 'aljshdkgasjsvpu98hqoubqevhinlf',
-            name: 'testing_username',
-            email: 'someemail@a.com',
-        };
-        login.state.username = 'testing_username';
-        login.state.password = '$@3sometest';
         const { wrapper } = build();
         expect(wrapper).toMatchSnapshot();
     });
 
     it('renders main child components', () => {
-        const {
-            name,
-            pass,
-            btn,
-            facebookIcon,
-            githubIcon,
-            googleIcon,
-        } = build();
-
-        expect(name().exists()).toBe(true);
-        expect(pass().exists()).toBe(true);
-        expect(btn().exists()).toBe(true);
+        const { facebookIcon, githubIcon, googleIcon } = build();
 
         expect(facebookIcon().exists()).toBe(true);
         expect(githubIcon().exists()).toBe(true);
         expect(googleIcon().exists()).toBe(true);
     });
 
-    it('passes vuex props to child components', () => {
-        const { state } = login;
-
-        const { name, pass, btn } = build();
-
-        expect(name().props().value).toBe(state.username);
-
-        expect(pass().props().value).toBe(state.password);
-
-        expect(btn().props().onclick).toBeInstanceOf(Function);
-    });
-
-    it('submit invalid login', async () => {
-        const { state, actions } = login;
-        state.username = '$#@¨&*(guyadshj';
-        const { btn } = build();
-        const { loginAction } = actions;
-
-        await btn().trigger('click');
-        expect(loginAction).not.toHaveBeenCalled();
-    });
-
-    it('submit valid login', async () => {
-        const { actions } = login;
-
-        const { btn } = build();
-        const { loginAction } = actions;
-        await btn().trigger('click');
-        // Used to force the waiting for the desired methods.
-        await setTimeout(() => expect(loginAction).toHaveBeenCalled(), 0);
-    });
-
-    it('calling clickLogin from vm', async () => {
-        const { actions } = login;
-        const { wrapperVM } = build();
-        const { loginAction } = actions;
-        await wrapperVM.clickLogin();
-        // Used to force the waiting for the desired methods.
-        await setTimeout(() => expect(loginAction).toHaveBeenCalled(), 0);
-    });
-
-    it('must have a password input typed', async () => {
-        const { pass } = build();
-        expect(pass().props().type).toBe('password');
-    });
-
-    it('must call facebook login action', async () => {
-        const { actions } = login;
-
+    it('must have href equal to facebook login route', async () => {
         const { facebookIcon } = build();
 
-        await facebookIcon().trigger('click');
-
-        expect(actions.facebookLogin).toBeCalled();
+        expect(facebookIcon().attributes('href')).toEqual(
+            `${baseURL}/auth/facebook`,
+        );
     });
-    it('must call github login action', async () => {
-        const { actions } = login;
-
+    it('must have href equal to google login route', async () => {
         const { githubIcon } = build();
-
-        await githubIcon().trigger('click');
-
-        expect(actions.githubLogin).toBeCalled();
+        expect(githubIcon().attributes('href')).toEqual(
+            `${baseURL}/auth/github`,
+        );
     });
-    it('must call google login action', async () => {
-        const { actions } = login;
 
+    it('must have href equal to google login route', async () => {
         const { googleIcon } = build();
 
-        await googleIcon().trigger('click');
-
-        expect(actions.googleLogin).toBeCalled();
+        expect(googleIcon().attributes('href')).toEqual(
+            `${baseURL}/auth/google`,
+        );
     });
 });
